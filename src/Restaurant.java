5b17316f2e8395a48efaf1322a6071ed615e1bd9
Shusmac <shusma.chhetri@gmail.com>
2024-12-14 18:37:50 -0500

    Adding Assignment 1

---
+++ src/Restaurant.java
@@ -0,0 +1,10 @@
+
+public class Restaurant {
+
+    public static void main(String[] args) {
+        // Print the restaurant information
+        System.out.println("Restaurant Name: Himalayan Yak");
+        System.out.println("Business Address: 72-20 Roosevelt Ave");
+        System.out.println("City, State, Zip: Jackson Heights, NY 11372");
+    }
+}
